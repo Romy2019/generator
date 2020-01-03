af090eb5c4059407f5f5ab959d73e06b08c271be
Romy2019 <romybobbyjohn@yahoo.com>
2020-01-03 11:24:26 +0530

    assignments edits

--- controller/user/login.js
+++ controller/user/login.js
@@ -1,6 +1,10 @@
 const addUser = require('../../models/userTable');
 const jwt = require('jsonwebtoken');
+const express = require('express');
 const TOKEN_SECRET = "D0dKsugPi5r}Viv";
+const BodyParser = require("body-parser");
+const Mongoose = require("mongoose");
+const Bcrypt = require("bcryptjs");
 
 function generateJWT(_id) {
     const token = jwt.sign({userId: _id}, TOKEN_SECRET);
@@ -8,40 +12,46 @@ function generateJWT(_id) {
 }
 
 exports.userLogIn = function (req, res) {
-    const userlog = req.body;
-    addUser.userTable.find({'userName': userlog.userName}, (err, docs) => {
-        console.log(docs);
+    if (!req.body.userName || !req.body.password) {
 
-        if (err) {
+        return res.status(500).send("object missing");
+    }
+    else {
+        const userlog = req.body;
+        addUser.userTable.find({'userName': userlog.userName}, (err, docs) => {
+            console.log(docs);
 
-            return res.status(500).send("Error getting user");
-        }
+            if (err) {
 
-        //if user found.
-        if (docs.length < 1 || docs == undefined||docs.length >1) {
-            res.status(401).send("invalid log in");//empty
-        } else {
-            if (docs[0].password === userlog.password) {
-                const token = generateJWT(docs[0]._id);
-                let result = {
-                    status: "success",
-                    data: {
-                        userName: userlog.userName,
-                        token: token
-                    }
-                };
-                res.status(200).send(result);
-            } else {
-                res.status(401).send("invalid log in");//empty
+                return res.status(500).send("Error getting user");
             }
 
 
-        }
+            //if user found.
+            if (docs.length < 1 || docs == undefined || docs.length > 1) {
+                res.status(401).send("invalid log in");//empty
+            } else {
+                if (Bcrypt.compareSync(userlog.password, docs[0].password)/*docs[0].password === userlog.password*/) {
+                    const token = generateJWT(docs[0]._id);
+                    let result = {
+                        status: " log in success",
+                        data: {
+                            userName: userlog.userName,
+                            token: token
+                        }
+                    };
+                    res.status(200).send(result);
 
-        // return results;//console.log('logged in user' + userlog.userName);
+                } else {
+                    res.status(401).send("invalid log in");//empty
+                }
 
 
-    });
+            }
 
+
+
+        });
+    }
 
 };
--- controller/user/signup.js
+++ controller/user/signup.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const addUser = require('../../models/userTable');
-
+const BodyParser = require("body-parser");
+const Mongoose = require("mongoose");
+const Bcrypt = require("bcryptjs");
 
 
 exports.userSignUp = function(req, res) {
-    const userAdd = new addUser.userTable(req.body);
+    if (!req.body.userName || !req.body.password || !req.body.userType) {
 
-    userAdd.save(function(err,success){
-      if(err){
-        res.send("error"+err)
-      }else{
+        return res.status(500).send("object missing");
+    }
+    else {
+        req.body.password = Bcrypt.hashSync(req.body.password, 10);
 
-        res.send('user created')
+        const userAdd = new addUser.userTable(req.body);
 
-      }
-    });
+        userAdd.save(function (err, success) {
+            if (err) {
+                res.send("error" + err)
+            } else {
 
+                res.send('user created')
 
+            }
+        });
+
+    }
 };
